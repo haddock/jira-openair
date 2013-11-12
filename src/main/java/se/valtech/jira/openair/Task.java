a60221fdd26e26956f631c6eb04b58d0e8bb377e
Fredrik Bergljung <fredrik.bergljung@valtech.se>
2013-11-12 17:08:21 +0100

    addded Open Air Task entity

---
+++ src/main/java/se/valtech/jira/openair/Task.java
@@ -0,0 +1,56 @@
+package se.valtech.jira.openair;
+
+import com.atlassian.jira.issue.Issue;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.StaxDriver;
+
+public class Task {
+	String id;
+	String created;
+	String updated;
+	String projecttask_typeid;
+	String userid;
+	String date;
+	String decimal_hours;
+	String cost_centerid;
+	String slipid;
+	String hours;
+	String timetypeid;
+	String minutes;
+	String projectid;
+	String description;
+	String categoryid;
+	String projecttaskid;
+	String timesheetid;
+	String notes;
+	String customerid;
+	String payroll_typeid;
+	String job_codeid;
+	String loaded_cost;
+	String loaded_cost_2;
+	String loaded_cost_3;
+	String project_loaded_cost;
+	String acct_date;
+	String category_1id;
+	String category_2id;
+	String category_3id;
+	String category_4id;
+	String cateogry_5id;
+	String thin_client_id;
+	
+	public Task(Issue issue) {
+		this.projectid = issue.getProjectObject().getDescription();
+		this.description = issue.getKey();
+		this.userid = issue.getAssigneeId();
+		this.created = issue.getCreated().toString();
+		this.updated = issue.getUpdated().toString();
+	}
+	
+	public String asXML () {
+		XStream xstream = new XStream(new StaxDriver());
+		xstream.aliasPackage("", "se.valtech.jira.openair");
+		return xstream.toXML(this);
+	}
+}
+
+
--- src/main/java/se/valtech/jira/plugins/IssueEventListener.java
+++ src/main/java/se/valtech/jira/plugins/IssueEventListener.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import se.valtech.jira.openair.Task;
+
 import com.atlassian.event.api.EventListener;
 import com.atlassian.event.api.EventPublisher;
 import com.atlassian.jira.event.issue.IssueEvent;
@@ -15,7 +17,6 @@ public class IssueEventListener implements InitializingBean, DisposableBean{
 
 	private static final Logger log = LoggerFactory.getLogger(IssueEventListener.class);
 	private final EventPublisher eventPublisher;
-	
 	
 	public IssueEventListener(EventPublisher eventPublisher) {
 		this.eventPublisher = eventPublisher;
@@ -36,15 +37,15 @@ public class IssueEventListener implements InitializingBean, DisposableBean{
 		Long eventTypeId = issueEvent.getEventTypeId();
 		Issue issue = issueEvent.getIssue();
 		
+		log.info(new Task(issue).asXML());
+		
+		log.info("Issue event ID: {} project: {}", issueEvent.getEventTypeId(), issue.getProjectObject().getDescription());
 		if (eventTypeId.equals(EventType.ISSUE_CREATED_ID)) {
-			log.info("Issue {} has been created at {}.", issue.getKey(), issue.getCreated());
-		} else if (eventTypeId.equals(EventType.ISSUE_RESOLVED_ID)) {
-			log.info("Issue {} has been resolved at {}.", issue.getKey(), issue.getResolutionDate());
-		} else if (eventTypeId.equals(EventType.ISSUE_CLOSED_ID)) {
-			log.info("Issue {} has been closed at {}.", issue.getKey(), issue.getUpdated());
-		} else if (eventTypeId.equals(EventType.ISSUE_REOPENED_ID)) {
-			log.info("Issue {} has been reopened at {}.", issue.getKey(), issue.getUpdated());
+			log.info("Issue {} has been created by {}.", issue.getKey(), issue.getReporter().getDisplayName());
+		} else if (eventTypeId.equals(EventType.ISSUE_ASSIGNED_ID)) {
+			log.info("Issue {} has been assigned to {}.", issue.getKey(), issue.getAssignee().getDisplayName());
+		} else if (eventTypeId.equals(EventType.ISSUE_DELETED_ID)) {
+			log.info("Issue {} has been deleted.", issue.getKey());
 		}
 	}
-
 }
