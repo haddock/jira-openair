7312561e8e81b9932bd5586634b66fcc90f70452
Fredrik Bergljung <fredrik.bergljung@valtech.se>
2013-11-20 21:18:41 +0100

    Get Open Air id:s from JIRA project & user

--- src/main/java/se/valtech/jira/plugins/IssueEventListener.java
+++ src/main/java/se/valtech/jira/plugins/IssueEventListener.java
@@ -1,30 +1,22 @@
 package se.valtech.jira.plugins;
 
-import java.io.IOException;
-
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.methods.PostMethod;
-import org.apache.commons.httpclient.methods.StringRequestEntity;
-import org.apache.commons.httpclient.params.HttpClientParams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-import se.valtech.jira.openair.Action;
-import se.valtech.jira.openair.Add;
-import se.valtech.jira.openair.Delete;
 import se.valtech.jira.openair.OpenAirCommunicationException;
 
+import com.atlassian.crowd.embedded.api.User;
 import com.atlassian.event.api.EventListener;
 import com.atlassian.event.api.EventPublisher;
+import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.event.issue.IssueEvent;
 import com.atlassian.jira.event.type.EventType;
 import com.atlassian.jira.issue.Issue;
 
 public class IssueEventListener implements InitializingBean, DisposableBean{
 
-	private static final String OPEN_AIR_URL = "http://httpbin.org/post";
 	private static final Logger log = LoggerFactory.getLogger(IssueEventListener.class);
 	private final EventPublisher eventPublisher;
 	
@@ -48,29 +40,14 @@ public class IssueEventListener implements InitializingBean, DisposableBean{
 		Issue issue = issueEvent.getIssue();
 		
 		if (eventTypeId.equals(EventType.ISSUE_CREATED_ID) || eventTypeId.equals(EventType.ISSUE_ASSIGNED_ID)) {
-			sendToOpenAir(new Add(issue));
-		} else if (eventTypeId.equals(EventType.ISSUE_DELETED_ID)) {
-			sendToOpenAir(new Delete(issue));
-		}
-	}
-
-	private void sendToOpenAir(Action action) throws OpenAirCommunicationException {
-		try {
-			HttpClient client = new HttpClient();
-			HttpClientParams params = new HttpClientParams();
-			params.setConnectionManagerTimeout(1000);
-			params.setSoTimeout(1000);
-			client.setParams(params);
-			PostMethod post = new PostMethod(OPEN_AIR_URL);
-			post.setRequestHeader("Content-Language", "en-US");
-			post.setRequestEntity(new StringRequestEntity(action.asXML(), "text/xml", "UTF-8"));
-			log.info(post.getRequestEntity().toString());
-			int responseCode = client.executeMethod(post);
-			if (responseCode >= 300) {
-				throw new OpenAirCommunicationException(new IOException("Error while sending " + post.getRequestEntity() + " to OpenAir at " + OPEN_AIR_URL + ". Got response code: " + responseCode));
-			}
-		} catch (Exception e) {
-			throw new OpenAirCommunicationException(e);
-		}
+			String description = issue.getProjectObject().getDescription();
+			int offset = description.indexOf("|openairid:") + "|openairid:".length();
+			int stop = description.indexOf("|", offset);
+			String openairprojectid = description.substring(offset, stop);
+			log.info("project id: " + openairprojectid);
+			User user = issue.getAssignee();
+			String openairuserid = ComponentAccessor.getUserPropertyManager().getPropertySet(user).getString("jira.meta.openairid");
+			log.info("user id: " + openairuserid);
+		} 
 	}
 }
